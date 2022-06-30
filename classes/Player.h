1da2b81e3def01d3418000f6afe28e362d50bdba
IraFidelino <59916454+IraFidelino@users.noreply.github.com>
2022-06-30 23:14:45 +0800

    Update Player.h

--- classes/Player.h
+++ classes/Player.h
@@ -6,7 +6,8 @@
 class Player: public Sprite {
 public:
     int health;
+    int collision;
 
-    Player(int width, int height, int x, int y, int xdir, int ydir, int health, FREE_IMAGE_FORMAT format, const char* filepath);
+    Player(int width, int height, int x, int y, int xdir, int ydir, int health, int collision, FREE_IMAGE_FORMAT format, const char* filepath);
   };
 #endif
