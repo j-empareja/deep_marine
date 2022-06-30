607eff9ffff516b5c637cfa3ca8a6f37c16b2bc9
IraFidelino <59916454+IraFidelino@users.noreply.github.com>
2022-06-30 15:38:58 +0800

    Add files via upload

---
+++ classes/Ammo.cpp
@@ -0,0 +1,22 @@
+#include "Ammo.h"
+#include "Sprite.h"
+#include <stdio.h>
+
+Ammo::Ammo(int width, int height, int x, int y, int xdir, int ydir, FREE_IMAGE_FORMAT format, const char* filepath) {
+    this->width = width;
+    this->height = height;
+    this->x = x;
+    this->x_old = x;
+    this->xdir = xdir;
+    this->y = y;
+    this->y_old = y;
+    this->ydir = ydir;
+
+    FIBITMAP* fi_asset = FreeImage_Load(format, filepath);
+    FreeImage_FlipVertical(fi_asset);
+    uint8_t* asset = FreeImage_GetBits(fi_asset);
+    this->img = asset;
+    this->pitch = FreeImage_GetPitch(fi_asset);
+
+    this->isFired = false;
+}
