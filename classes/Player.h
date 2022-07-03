b12c95da5b50a06c1ada8caf63abe649cb73111e
IraFidelino <59916454+IraFidelino@users.noreply.github.com>
2022-07-04 01:52:58 +0800

    Update Player.h

--- classes/Player.h
+++ classes/Player.h
@@ -3,11 +3,13 @@
 
 #include "Sprite.h"
 
-class Player: public Sprite {
+class Player : public Sprite {
 public:
     int health;
     int collision;
+    int sprite_var;
+    uint8_t* sprites[12];
 
-    Player(int width, int height, int x, int y, int xdir, int ydir, int health, int collision, FREE_IMAGE_FORMAT format, const char* filepath);
-  };
+    Player(int width, int height, int x, int y, int xdir, int ydir, int health, int collision, FREE_IMAGE_FORMAT format);
+};
 #endif
