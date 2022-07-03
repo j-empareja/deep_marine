9e5ec0c0791eef7ff2cee52beb8a0a5f9f4a621e
IraFidelino <59916454+IraFidelino@users.noreply.github.com>
2022-07-03 11:28:34 +0800

    Update Sprite.cpp

--- classes/Sprite.cpp
+++ classes/Sprite.cpp
@@ -11,15 +11,17 @@ Sprite::Sprite(int width, int height, int x, int y, int xdir, int ydir, bool isV
     this->x_old = x;
     this->x_initial = x;
     this->xdir = xdir;
+    this->xdir_initial = xdir;
     this->y = y;
     this->y_old = y;
     this->y_initial = y;
     this->ydir = ydir;
+    this->ydir_initial = ydir;
     this->isVisible = isVisible;
 
     FIBITMAP* fi_asset = FreeImage_Load(format, filepath);
     FreeImage_FlipVertical(fi_asset);
     uint8_t* asset = FreeImage_GetBits(fi_asset);
     this->img = asset;
-    this->pitch = FreeImage_GetPitch(fi_asset);   
+    this->pitch = FreeImage_GetPitch(fi_asset);
 }
